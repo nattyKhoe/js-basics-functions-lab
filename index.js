f773077a5dabd104ca3f17b5f25749fca98503f2
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-22 22:15:53 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Code your solution in this file!//
+//loc0=42//
+function distanceFromHqInBlocks (loc1){
+    return Math.abs(42-loc1);
+}
+
+function distanceFromHqInFeet (loc1){
+    return distanceFromHqInBlocks(loc1)*264;
+}
+function distanceTravelledInFeet (loc1, loc2){
+    return Math.abs(loc1-loc2)*264;
+}
+
+function calculatesFarePrice (start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    switch (true) {
+        case distance<= 400:
+            return 0;
+            break;
+        case distance<=2000:
+            return (distance-400)*0.02;
+            break;
+        case distance<=2500:
+            return 25;
+            break;
+        case distance>2500:
+            return `cannot travel that far`;
+            break;
+    }
+}
